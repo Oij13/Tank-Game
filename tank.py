79a070f1854dbd7f13f4b599039993ebf8651038
Oij13 <156932008+Oij13@users.noreply.github.com>
2024-04-12 18:46:21 -0400

    Update tank.py

--- tank.py
+++ tank.py
@@ -13,23 +13,44 @@ class Tank1(simpleGE.Sprite):
     def __init__(self, scene):
         super().__init__(scene)
         self.setImage("Tank.png")
-        self.setSize(30,30)
+        self.setSize(40,40)
+        self.position = (60,250)
         
     
     def process(self):
         if self.isKeyPressed(pygame.K_a):
-            self.turnBy(5)
+            self.turnBy(3)
         if self.isKeyPressed(pygame.K_d):
-            self.turnBy(-5)
+            self.turnBy(-3)
         if self.isKeyPressed(pygame.K_w):
-            self.forward(5)
+            self.forward(3)
         if self.isKeyPressed(pygame.K_s):
             self.forward(-3)
 
 
 
 
-class Bullet(simpleGE.Sprite):
+
+class Tank2(simpleGE.Sprite):
+    def __init__(self, scene):
+        super().__init__(scene)
+        self.setImage("Redtank.png")
+        self.setSize(40,40)
+        self.position = (560,250)
+        
+    
+    def process(self):
+        if self.isKeyPressed(pygame.K_j):
+            self.turnBy(3)
+        if self.isKeyPressed(pygame.K_l):
+            self.turnBy(-3)
+        if self.isKeyPressed(pygame.K_i):
+            self.forward(-3)
+        if self.isKeyPressed(pygame.K_k):
+            self.forward(3)
+
+
+class Bullet1(simpleGE.Sprite):
     def __init__(self, scene, parent):
         super().__init__(scene)
         self.parent = parent
@@ -46,25 +67,67 @@ class Bullet(simpleGE.Sprite):
             self.speed = 20
 
     
+class Bullet2(simpleGE.Sprite):
+    def __init__(self, scene, parent):
+        super().__init__(scene)
+        self.parent = parent
+        self.setImage("Bullet.png")
+        self.setSize(5,5)
+        self.setBoundAction(self.HIDE)
+        self.hide()
 
+    def fire(self):
+        if not self.visible:
+            self.show()
+            self.position = self.parent.position
+            self.moveAngle = self.parent.imageAngle
+            self.speed = -20
 
 
 class Game(simpleGE.Scene):
     def __init__(self):
         super().__init__()
-        self.tank = Tank1(self)
-        self.bullet = Bullet(self, self.tank)
-        self.sprites = [self.tank, self.bullet]
+        
+        self.tank1 = Tank1(self)
+        self.bullet1 = Bullet1(self, self.tank1)
+        self.setImage("RockBG.png")
+        self.tank2 = Tank2(self)
+        self.bullet2 = Bullet2(self, self.tank2)
+        
+        self.barriers = [Barrier(self, (325, 470)),
+                         Barrier(self, (325, 10))]
 
+        self.sprites = [self.tank1, self.bullet1, self.tank2, self.bullet2, self.barriers]
+        
+        
     def processEvent(self, event):
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                self.bullet.fire()        
+            if event.key == pygame.K_f:
+                self.bullet1.fire()        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_h:
+                self.bullet2.fire()    
         
+    
+
+
+
+class Barrier(simpleGE.Sprite):
+    def __init__(self, scene, position):
+        super().__init__(scene)
+        self.position = (position)
+        self.colorRect("yellow", (650, 25))
         
-        
-        
-        
+
+
+
+
+
+
+
+
+
+
 def main():
     game = Game()
     game.start()
