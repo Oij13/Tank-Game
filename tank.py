c99116387873967036f8f6ba743d447e49d3ea8f
Oij13 <156932008+Oij13@users.noreply.github.com>
2024-04-12 16:36:26 -0400

    Update tank.py

--- tank.py
+++ tank.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Apr 12 11:09:09 2024
 
-@author: cmj17
-"""
 
 # -*- coding: utf-8 -*-
 """
@@ -20,9 +15,6 @@ class Tank1(simpleGE.Sprite):
         self.setImage("Tank.png")
         self.setSize(30,30)
         
-        self.gun = Gun(self.scene, self)
-        self.gun.setPosition(0, -15)
-    
     
     def process(self):
         if self.isKeyPressed(pygame.K_a):
@@ -52,15 +44,7 @@ class Bullet(simpleGE.Sprite):
         self.moveAngle = self.parent.imageAngle
         self.speed = 20
 
-
-class Gun(simpleGE.Sprite):
-    def __init__(self, scene, parent):
-        super().__init__(scene)
-        self.parent = parent
-        self.setImage("GunTurret.png")
-        self.setBoundAction(self.HIDE)
-        
-
+    
 
 
 
@@ -68,10 +52,6 @@ class Game(simpleGE.Scene):
     def __init__(self):
         super().__init__()
         self.tank = Tank1(self)
-        self.gun = Gun(self)
-        
-        self.sprites = [self.tank, self.gun]
-
         self.NUM_BULLETS = 100
         self.currentBullet = 0
         self.bullets = []
@@ -81,8 +61,9 @@ class Game(simpleGE.Scene):
         self.sprites = [self.tank, self.bullets]
 
     def processEvent(self, event):
+        super().processEvent(event)
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
+            if event.key == pygame.K_f:
                 self.currentBullet += 1
                 if self.currentBullet >= self.NUM_BULLETS:
                     self.currentBullet = 0
